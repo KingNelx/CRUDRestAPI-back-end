ee3698d3ba5409d97146309c95f72b2c6b83ae04
KingNelx <tapiajonel606@gmail.com>
2022-11-03 13:51:08 +0800

    added mapping

--- crud-be/src/main/java/crudbe/Repository/PatientRepo.java
+++ crud-be/src/main/java/crudbe/Repository/PatientRepo.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface PatientRepo extends JpaRepository <Patients, Long> {
 
-    @Query("SELECT * FROM Patient Patient WHERE gender = male")
-    List<String> getTest();
+//    @Query("SELECT * FROM Patient Patient WHERE gender = male")
+//    List<String> getTest();
 }
