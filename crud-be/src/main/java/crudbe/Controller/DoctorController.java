47729bae29f4f7b86c8a17556a615ab68ea53293
KingNelx <tapiajonel606@gmail.com>
2022-11-04 09:37:45 +0800

    added cross origin

--- crud-be/src/main/java/crudbe/Controller/DoctorController.java
+++ crud-be/src/main/java/crudbe/Controller/DoctorController.java
@@ -13,6 +13,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/doctor")
+@CrossOrigin("http://localhost:3000/")
+
 public class DoctorController {
 
     @Autowired
--- crud-be/src/main/java/crudbe/Controller/RHUController.java
+++ crud-be/src/main/java/crudbe/Controller/RHUController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/rhu")
+@CrossOrigin("http://localhost:3000/")
 public class RHUController {
 
     @Autowired
